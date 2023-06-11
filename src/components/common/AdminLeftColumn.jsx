cfd6121f083eddaf12f2122770ccd8d761c8d73c
Samuel Lee <mcukyo@gmail.com>
2023-06-12 01:23:18 +0800

    Feat: Add functions to pages and fix icon image (see details in the description below)
    Add delete function to AdminMainPage
    Re-arrange possible alert messages to signup / login pages
    Add word count function to input element
    Add :focus, :hover, :active effect to elements
    Made icon color switchable in AdminLeftColumn

--- src/components/common/AdminLeftColumn.jsx
+++ src/components/common/AdminLeftColumn.jsx
@@ -1,26 +1,42 @@
 import { ReactComponent as Logo } from "../../assets/icons/LOGO.svg";
-import { ReactComponent as HomeSelected } from "../../assets/icons/home_selected.svg";
-import { ReactComponent as Profile } from "../../assets/icons/profile.svg";
+// import { ReactComponent as HomeSelected } from "../../assets/icons/home_selected.svg";
+// import { ReactComponent as Profile } from "../../assets/icons/profile.svg";
 import { ReactComponent as Logout } from "../../assets/icons/Logout.svg";
+// import { ReactComponent as ProfileSelected } from "../../assets/icons/profile_selected.svg";
+import { Link } from "react-router-dom";
 
-const NavItem = ({ icon, title }) => {
+const NavItem = ({ icon, title, onclick }) => {
   return (
-    <div className="adminNavItemContainer">
+    <div 
+      className="adminNavItemContainer" 
+      // onClick={onclick}
+    >
       <div className="adminNavIconContainer">{icon}</div>
       <div className="adminNavTitle">{title}</div>
     </div>
   );
 };
 
-const AdminLeftColumn = () => {
+const AdminLeftColumn = ({ icon1, icon2 }) => {
+
   return (
     <div className="adminLeftColumn">
       <div className="adminLeftColumnTop">
         <div className="adminLeftColumnLogoContainer">
           <Logo />
         </div>
-        <NavItem icon={<HomeSelected />} title="推文清單"></NavItem>
-        <NavItem icon={<Profile />} title="使用者列表"></NavItem>
+        <Link to="/admin/main">
+          <NavItem
+            icon={icon1}
+            title="推文清單"
+          ></NavItem>
+        </Link>
+        <Link to="/admin/users">
+          <NavItem
+            icon={icon2}
+            title="使用者列表"
+          ></NavItem>
+        </Link>
       </div>
       <NavItem icon={<Logout />} title="登出"></NavItem>
     </div>
