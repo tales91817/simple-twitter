963bb6bbd535d4e93c1f2743f24dc53c2ef7d2cc
Samuel Lee <mcukyo@gmail.com>
2023-06-12 02:16:05 +0800

    refactor: delete unneeded code

--- src/components/common/AdminLeftColumn.jsx
+++ src/components/common/AdminLeftColumn.jsx
@@ -1,16 +1,10 @@
 import { ReactComponent as Logo } from "../../assets/icons/LOGO.svg";
-// import { ReactComponent as HomeSelected } from "../../assets/icons/home_selected.svg";
-// import { ReactComponent as Profile } from "../../assets/icons/profile.svg";
 import { ReactComponent as Logout } from "../../assets/icons/Logout.svg";
-// import { ReactComponent as ProfileSelected } from "../../assets/icons/profile_selected.svg";
 import { Link } from "react-router-dom";
 
-const NavItem = ({ icon, title, onclick }) => {
+const NavItem = ({ icon, title }) => {
   return (
-    <div 
-      className="adminNavItemContainer" 
-      // onClick={onclick}
-    >
+    <div className="adminNavItemContainer" >
       <div className="adminNavIconContainer">{icon}</div>
       <div className="adminNavTitle">{title}</div>
     </div>
@@ -26,16 +20,16 @@ const AdminLeftColumn = ({ icon1, icon2 }) => {
           <Logo />
         </div>
         <Link to="/admin/main">
-          <NavItem
-            icon={icon1}
-            title="推文清單"
-          ></NavItem>
+          <NavItem 
+            icon={icon1} 
+            title="推文清單" 
+          />
         </Link>
         <Link to="/admin/users">
-          <NavItem
-            icon={icon2}
-            title="使用者列表"
-          ></NavItem>
+          <NavItem 
+            icon={icon2} 
+            title="使用者列表" 
+          />
         </Link>
       </div>
       <NavItem icon={<Logout />} title="登出"></NavItem>
