a34cb4ce48b70c98c52424b767296318a726fa95
Samuel Lee <mcukyo@gmail.com>
2023-06-13 22:53:17 +0800

    Feat: add login/logout and register functions (direct to HomePage after login)

--- src/components/MainPageFunctions/Sidebar/Sidebar.jsx
+++ src/components/MainPageFunctions/Sidebar/Sidebar.jsx
@@ -19,48 +19,73 @@ const Sidebar = () => {
         setIsSelectedIcon(buttonName)
     }
     const navigate = useNavigate()
+    const handleClick = () => {
+      localStorage.removeItem("authToken");
+      navigate("/users/login");
+    };
 
   return (
-    <div className='sidebar'>
-        <div className="btn-wrapper">
-            <div className='logo'>
-                <a href="#"><LOGO className='icons' /></a>
-            </div>
-            <div className={isSelectedIcon === 'home' ? 'selected' : 'item'} 
-                onClick={() => {
-                    handleButtonClick('home')
-                    navigate('/')
-                }}>
-                {isSelectedIcon === 'home' ? <HomeIconSelected className='icons' /> : <HomeIcon className='icons' />}
-                <div className="text">首頁</div>
-            </div>
-            <div className={isSelectedIcon === 'profile' ? 'selected' : 'item'} 
-                onClick={() => {
-                    handleButtonClick('profile')
-                    navigate('/profile')
-                }}>
-                {isSelectedIcon ==='profile' ? <ProfileIconSelected className="icons" /> : <ProfileIcon className='icons' />}
-                <div className="text">個人資料</div>
-            </div>
-            <div className={isSelectedIcon === 'setting' ? 'selected' : 'item'} 
-                onClick={() => {
-                    handleButtonClick('setting')
-                    navigate('/setting')
-                }}>
-                {isSelectedIcon === 'setting' ? <SettingIconSelected className='icons' /> : <SettingIcon className='icons' />}
-                <div className="text">設定</div>
-            </div>
-            <div className="sidebar_Btn">
-                <a href="#">推文</a>
-            </div>
+    <div className="sidebar">
+      <div className="btn-wrapper">
+        <div className="logo">
+          <a href="#">
+            <LOGO className="icons" />
+          </a>
         </div>
-        <div className="logout">
-            <div className="logoutBtn">
-                <LogoutIcon /><div className='logoutText'>登出</div>
-            </div>
+        <div
+          className={isSelectedIcon === "home" ? "selected" : "item"}
+          onClick={() => {
+            handleButtonClick("home");
+            navigate("/");
+          }}
+        >
+          {isSelectedIcon === "home" ? (
+            <HomeIconSelected className="icons" />
+          ) : (
+            <HomeIcon className="icons" />
+          )}
+          <div className="text">首頁</div>
         </div>
+        <div
+          className={isSelectedIcon === "profile" ? "selected" : "item"}
+          onClick={() => {
+            handleButtonClick("profile");
+            navigate("/profile");
+          }}
+        >
+          {isSelectedIcon === "profile" ? (
+            <ProfileIconSelected className="icons" />
+          ) : (
+            <ProfileIcon className="icons" />
+          )}
+          <div className="text">個人資料</div>
+        </div>
+        <div
+          className={isSelectedIcon === "setting" ? "selected" : "item"}
+          onClick={() => {
+            handleButtonClick("setting");
+            navigate("/setting");
+          }}
+        >
+          {isSelectedIcon === "setting" ? (
+            <SettingIconSelected className="icons" />
+          ) : (
+            <SettingIcon className="icons" />
+          )}
+          <div className="text">設定</div>
+        </div>
+        <div className="sidebar_Btn">
+          <a href="#">推文</a>
+        </div>
+      </div>
+      <div onClick={handleClick} className="logout">
+        <div className="logoutBtn">
+          <LogoutIcon />
+          <div className="logoutText">登出</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Sidebar
--- src/components/common/AdminLeftColumn.jsx
+++ src/components/common/AdminLeftColumn.jsx
@@ -1,10 +1,10 @@
 import { ReactComponent as Logo } from "../../assets/icons/LOGO.svg";
 import { ReactComponent as Logout } from "../../assets/icons/Logout.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const NavItem = ({ icon, title }) => {
+const NavItem = ({ icon, title, onClick }) => {
   return (
-    <div className="adminNavItemContainer" >
+    <div onClick={onClick} className="adminNavItemContainer">
       <div className="adminNavIconContainer">{icon}</div>
       <div className="adminNavTitle">{title}</div>
     </div>
@@ -12,6 +12,12 @@ const NavItem = ({ icon, title }) => {
 };
 
 const AdminLeftColumn = ({ icon1, icon2 }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+      localStorage.removeItem("authToken");
+      navigate("/admin/login");
+    };
 
   return (
     <div className="adminLeftColumn">
@@ -19,20 +25,14 @@ const AdminLeftColumn = ({ icon1, icon2 }) => {
         <div className="adminLeftColumnLogoContainer">
           <Logo />
         </div>
-        <Link to="/admin/main">
-          <NavItem 
-            icon={icon1} 
-            title="推文清單" 
-          />
+        <Link className="navLink" to="/admin/main">
+          <NavItem icon={icon1} title="推文清單" />
         </Link>
-        <Link to="/admin/users">
-          <NavItem 
-            icon={icon2} 
-            title="使用者列表" 
-          />
+        <Link className="navLink" to="/admin/users">
+          <NavItem icon={icon2} title="使用者列表" />
         </Link>
       </div>
-      <NavItem icon={<Logout />} title="登出"></NavItem>
+      <NavItem onClick={handleClick} icon={<Logout />} title="登出"></NavItem>
     </div>
   );
 };
