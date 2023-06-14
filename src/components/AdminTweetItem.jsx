a4e779d49b587bc6cf593c74820b70c3ec13fd61
Samuel Lee <mcukyo@gmail.com>
2023-06-14 18:11:33 +0800

    Fix: fix slice error of tweet.description and change post time form

--- src/components/AdminTweetItem.jsx
+++ src/components/AdminTweetItem.jsx
@@ -1,10 +1,25 @@
 const AdminTweetItem = ( { tweet , onDelete } ) => {
   const deleteImagePath = require("../assets/icons/delete.svg").default;
 
+  const getTimeDifference = (tweetTimestamp) => {
+    const tweetDate = new Date(tweetTimestamp);
+    const currentDate = new Date();
+
+    const timeDifference = Math.abs(currentDate - tweetDate); // 獲取兩個日期的毫秒數差距
+    const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60)); // 轉換為小時
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); // 轉換為天數
+
+    if (daysDifference > 0) {
+      return `${daysDifference} 天前`;
+    } else {
+      return `${hoursDifference} 小時前`;
+    }
+  };
+  
   let shortenedContent =''
   if (tweet.description.length > 50) {
     shortenedContent = tweet.description.slice(0, 50) + "...";
-  }
+  }else shortenedContent = tweet.description;
 
   return (
     <div className="adminTweetItemWrapper">
@@ -15,7 +30,7 @@ const AdminTweetItem = ( { tweet , onDelete } ) => {
         <div className="tweetInfo">
           <div className="tweetName">{tweet.name}</div>
           <div className="tweetOtherInfo">
-            ＠{tweet.account}・{tweet.createdAt}
+            ＠{tweet.account}・{getTimeDifference(tweet.createdAt)}
           </div>
           <div
             className="deleteButton"
